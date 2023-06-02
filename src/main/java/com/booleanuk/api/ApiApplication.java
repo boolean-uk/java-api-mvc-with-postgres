0197e82bdc5a847525fa6a66241876d3f0fef710
Sebastian Andrisan <sebyyyandr@gmail.com>
2023-06-02 14:47:17 +0300

    api application run

---
+++ src/main/java/com/booleanuk/api/ApiApplication.java
@@ -0,0 +1,9 @@
+package com.booleanuk.api;
+
+import org.springframework.boot.SpringApplication;
+
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
