fad84c4d71eeae2f5852a0908eef1bc904a76205
Rose Breedveld <rosebreedveld@hotmail.com>
2023-06-02 08:56:45 -0400

    refactored code

--- src/main/java/com/booleanuk/api/models/Employee.java
+++ src/main/java/com/booleanuk/api/models/Employee.java
@@ -1,4 +1,69 @@
 package com.booleanuk.api.models;
 
 public class Employee {
+
+    private long  id;
+    private String name;
+    private String jobName;
+    private String salaryGrade;
+    private String department;
+    private String phone;
+
+    public Employee(long id, String name, String jobName, String salaryGrade, String department, String phone) {
+        this.id = id;
+        this.name = name;
+        this.jobName = jobName;
+        this.salaryGrade = salaryGrade;
+        this.department = department;
+        this.phone = phone;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJobName() {
+        return jobName;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+    public String getSalaryGrade() {
+        return salaryGrade;
+    }
+
+    public void setSalaryGrade(String salaryGrade) {
+        this.salaryGrade = salaryGrade;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
--- src/main/java/com/booleanuk/api/repositories/EmployeeRepository.java
+++ src/main/java/com/booleanuk/api/repositories/EmployeeRepository.java
@@ -3,109 +3,112 @@ package com.booleanuk.api.repositories;
 import com.booleanuk.api.data.DbAccess;
 import com.booleanuk.api.models.Employee;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeRepository {
 
-//    private DbAccess dbAcces;
+    private DbAccess dbAcces;
+    private Connection employee;
+    private Connection connection;
+
+    public EmployeeRepository() throws SQLException {
+        this.dbAcces = new DbAccess();
+    }
+
+    public List<Employee> getAll() throws SQLException {
+        List<Employee> everyone = new ArrayList<>();
+        PreparedStatement statement = this.dbAcces.getAccess().prepareStatement("SELECT * FROM Employee");
+
+        ResultSet results = statement.executeQuery();
+
+        while (results.next()) {
+            Employee theEmployee = new Employee(
+                    results.getLong("id"),
+                    results.getString("name"),
+                    results.getString("jobName"),
+                    results.getString("salaryGrade"),
+                    results.getString("department"), results.getString("phone"));
+            everyone.add(theEmployee);
+        }
+        return everyone;
+    }
+
+    public Employee get(long id) throws SQLException {
+        PreparedStatement statement = this.employee.prepareStatement("SELECT * FROM Customers WHERE id = ?");
+        // Choose set**** matching the datatype of the missing element
+        statement.setLong(1, id);
+        ResultSet results = statement.executeQuery();
+        Employee employee = null;
+        if (results.next()) {
+            employee = new Employee(results.getLong("id"), results.getString("name"), results.getString("jobName"), results.getString("salaryGrade"), results.getString("department"), results.getString("phone"));
+        }
+        return employee;
+    }
 //
-//    public EmployeeRepository() throws SQLException {
-//        this.dbAcces = new DbAccess();
-//    }
-//
-//    public List<Employee> getAll() throws SQLException {
-//        List<Employee> everyone = new ArrayList<>();
-//        PreparedStatement statement = this.dbAcces.getAccess().prepareStatement("SELECT * FROM Employee");
-//
-//        ResultSet results = statement.executeQuery();
-//
-//        while (results.next()) {
-//            Employee theEmployee = new Employee(
-//                    results.getLong("id"),
-//                    results.getString("name"),
-//                    results.getString("address"),
-//                    results.getString("email"),
-//                    results.getString("phone"));
-//            everyone.add(theCustomer);
-//        }
-//        return everyone;
-//    }
-//
-//    public Customer get(long id) throws SQLException {
-//        PreparedStatement statement = this.connection.prepareStatement("SELECT * FROM Customers WHERE id = ?");
-//        // Choose set**** matching the datatype of the missing element
-//        statement.setLong(1, id);
-//        ResultSet results = statement.executeQuery();
-//        Customer customer = null;
-//        if (results.next()) {
-//            customer = new Customer(results.getLong("id"), results.getString("name"), results.getString("address"), results.getString("email"), results.getString("phone"));
-//        }
-//        return customer;
-//    }
-//
-//    public Customer update(long id, Customer customer) throws SQLException {
-//        String SQL = "UPDATE Customers " +
-//                "SET name = ? ," +
-//                "address = ? ," +
-//                "email = ? ," +
-//                "phone = ? " +
-//                "WHERE id = ? ";
-//        PreparedStatement statement = this.connection.prepareStatement(SQL);
-//        statement.setString(1, customer.getName());
-//        statement.setString(2, customer.getAddress());
-//        statement.setString(3, customer.getEmail());
-//        statement.setString(4, customer.getPhoneNumber());
-//        statement.setLong(5, id);
-//        int rowsAffected = statement.executeUpdate();
-//        Customer updatedCustomer = null;
-//        if (rowsAffected > 0) {
-//            updatedCustomer = this.get(id);
-//        }
-//        return updatedCustomer;
-//    }
-//
-//    public Customer delete(long id) throws SQLException {
-//        String SQL = "DELETE FROM Customers WHERE id = ?";
-//        PreparedStatement statement = this.connection.prepareStatement(SQL);
-//        // Get the customer we're deleting before we delete them
-//        Customer deletedCustomer = null;
-//        deletedCustomer = this.get(id);
-//
-//        statement.setLong(1, id);
-//        int rowsAffected = statement.executeUpdate();
-//        if (rowsAffected == 0) {
-//            //Reset the customer we're deleting if we didn't delete them
-//            deletedCustomer = null;
-//        }
-//        return deletedCustomer;
-//    }
-//
-//    public Customer add(Customer customer) throws SQLException {
-//        String SQL = "INSERT INTO Customers(name, address, email, phone) VALUES(?, ?, ?, ?)";
-//        PreparedStatement statement = this.connection.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
-//        statement.setString(1, customer.getName());
-//        statement.setString(2, customer.getAddress());
-//        statement.setString(3, customer.getEmail());
-//        statement.setString(4, customer.getPhoneNumber());
-//        int rowsAffected = statement.executeUpdate();
-//        long newId = 0;
-//        if (rowsAffected > 0) {
-//            try (ResultSet rs = statement.getGeneratedKeys()) {
-//                if (rs.next()) {
-//                    newId = rs.getLong(1);
-//                }
-//            } catch (Exception e) {
-//                System.out.println("Oops: " + e);
-//            }
-//            customer.setId(newId);
-//        } else {
-//            customer = null;
-//        }
-//        return customer;
-//    }
+    public Employee update(long id, Employee employee) throws SQLException {
+        String SQL = "UPDATE Employee " +
+                "SET name = ? ," +
+                "jobName = ? ," +
+                "salaryGrade = ? ," +
+                "department = ? " +
+                "phone = ? " +
+                "WHERE id = ? ";
+        PreparedStatement statement = this.connection.prepareStatement(SQL);
+        statement.setString(1, employee.getName());
+        statement.setString(2, employee.getJobName());
+        statement.setString(3, employee.getSalaryGrade());
+        statement.setString(4, employee.getDepartment());
+        statement.setString(5, employee.getPhone());
+        statement.setLong(6, id);
+        int rowsAffected = statement.executeUpdate();
+        Employee updatedEmployee = null;
+        if (rowsAffected > 0) {
+            updatedEmployee = this.get(id);
+        }
+        return updatedEmployee;
+    }
+
+
+    public Employee delete(long id) throws SQLException {
+        String SQL = "DELETE FROM Customers WHERE id = ?";
+        PreparedStatement statement = this.connection.prepareStatement(SQL);
+        // Get the employee we're deleting before we delete them
+        Employee deletedEmployee = null;
+        deletedEmployee = this.get(id);
+
+        statement.setLong(1, id);
+        int rowsAffected = statement.executeUpdate();
+        if (rowsAffected == 0) {
+            //Reset the employee we're deleting if we didn't delete them
+            deletedEmployee = null;
+        }
+        return deletedEmployee;
+    }
+
+    public Employee add(Employee employee) throws SQLException {
+        String SQL = "INSERT INTO Customers(name, address, email, phone) VALUES(?, ?, ?, ?)";
+        PreparedStatement statement = this.connection.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+        statement.setString(1, employee.getName());
+        statement.setString(2, employee.getJobName());
+        statement.setString(3, employee.getSalaryGrade());
+        statement.setString(4, employee.getDepartment());
+        statement.setString(5, employee.getPhone());
+        int rowsAffected = statement.executeUpdate();
+        long newId = 0;
+        if (rowsAffected > 0) {
+            try (ResultSet rs = statement.getGeneratedKeys()) {
+                if (rs.next()) {
+                    newId = rs.getLong(1);
+                }
+            } catch (Exception e) {
+                System.out.println("Oops: " + e);
+            }
+            employee.setId(newId);
+        } else {
+            employee = null;
+        }
+        return employee;
+    }
 }
