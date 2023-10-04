aba2d0a54f4a2c63324f9f19ce26fe99072fa1f0
iliask796 <iliaskal7@gmail.com>
2023-10-04 16:14:03 +0300

    db: create employees table + dummy data

---
+++ db/migrations/V1.1__populate_employees_with_dummy_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO employees
+    (name, jobName, salaryGrade, department)
+VALUES
+    ('Rosemary Maxwell', 'React Developer', '9', 'Software Development'),
+    ('Dillon Payne', 'Java Developer', '8', 'Software Development'),
+    ('Sam Ramirez', 'Business Analyst', '6', 'Product Management'),
+    ('Jorja Mcneil', 'Tester', '6', 'Quality Assurance'),
+    ('Brenda Carpenter', 'SQL Developer', '7', 'Software Development');
