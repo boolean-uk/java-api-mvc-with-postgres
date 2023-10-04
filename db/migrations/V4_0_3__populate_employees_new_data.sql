84a38f071293ad28d943b83c7fd41ffc44231fe1
Petros Moustakis <petrosmoukas16@gmail.com>
2023-10-04 17:45:04 +0300

    modify schema

---
+++ db/migrations/V4_0_3__populate_employees_new_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO employees (fullname, jobName, department_id, salary_id) VALUES
+    ('John Doe', 'Software Engineer', 43, 41),
+    ('Jane Smith', 'Product Manager', 47, 40),
+    ('Alice Johnson', 'Data Scientist', 48, 36),
+    ('Bob Brown', 'HR Specialist', 46, 38),
+    ('Charlie Davis', 'Network Administrator', 44, 37),
+    ('Eve Adams', 'Accountant', 45, 39);
+
