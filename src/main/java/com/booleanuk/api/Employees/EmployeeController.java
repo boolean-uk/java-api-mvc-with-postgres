f8462ad4fe5aca23fe60222bd711b264aa775a77
iliask796 <iliaskal7@gmail.com>
2023-10-04 18:40:37 +0300

    all employee endpoints done with all http error statuses

--- src/main/java/com/booleanuk/api/Employees/EmployeeController.java
+++ src/main/java/com/booleanuk/api/Employees/EmployeeController.java
@@ -1,7 +1,12 @@
 package com.booleanuk.api.Employees;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.sql.SQLException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
@@ -10,5 +15,47 @@ public class EmployeeController {
 
     public EmployeeController() {
         employees = new EmployeeRepository();
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() throws SQLException {
+        return this.employees.getAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee postEmployee(@RequestBody Employee employee) throws SQLException {
+        Employee theEmployee = this.employees.create(employee);
+        if (theEmployee == null) {
+            throw new ResponseStatusException(HttpStatusCode.valueOf(400), "Could not create employee, please check all required fields are correct.");
+        }
+        return employee;
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployee(@PathVariable int id) throws SQLException {
+        Employee theEmployee = this.employees.get(id);
+        if (theEmployee == null) {
+            throw new ResponseStatusException(HttpStatusCode.valueOf(404), "No employees with that id were found.");
+        }
+        return theEmployee;
+    }
+
+    @PutMapping("/{id}")
+    public Employee updateEmployee(@PathVariable int id, @RequestBody Employee employee) throws SQLException {
+        Employee theEmployee = this.employees.update(id, employee);
+        if (theEmployee == null) {
+            throw new ResponseStatusException(HttpStatusCode.valueOf(404), "No employee with that id was found.");
+        }
+        return theEmployee;
+    }
+
+    @DeleteMapping("/{id}")
+    public Employee deleteEmployee(@PathVariable int id) throws SQLException {
+        Employee theEmployee = this.employees.delete(id);
+        if (theEmployee == null) {
+            throw new ResponseStatusException(HttpStatusCode.valueOf(404), "No employee with that id was found.");
+        }
+        return theEmployee;
     }
 }
--- src/main/java/com/booleanuk/api/Employees/EmployeeRepository.java
+++ src/main/java/com/booleanuk/api/Employees/EmployeeRepository.java
@@ -1,12 +1,15 @@
 package com.booleanuk.api.Employees;
 
 import org.postgresql.ds.PGSimpleDataSource;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.sql.DataSource;
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class EmployeeRepository {
@@ -44,5 +47,89 @@ public class EmployeeRepository {
         final PGSimpleDataSource dataSource = new PGSimpleDataSource();
         dataSource.setUrl(url);
         return dataSource;
+    }
+
+    public List<Employee> getAll() throws SQLException{
+        List<Employee> employees = new ArrayList<>();
+        PreparedStatement statement = this.connection.prepareStatement("SELECT * FROM employees");
+        ResultSet resultSet = statement.executeQuery();
+        while (resultSet.next()){
+            employees.add(new Employee(
+                    resultSet.getInt("id"),
+                    resultSet.getString("name"),
+                    resultSet.getString("jobName"),
+                    resultSet.getString("salaryGrade"),
+                    resultSet.getString("department")
+            ));
+        }
+        return employees;
+    }
+
+    public Employee create(Employee employee) throws SQLException{
+        String SQL = "INSERT INTO employees (name, jobName, salaryGrade, department) VALUES (?, ?, ?, ?)";
+        PreparedStatement statement = this.connection.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+        statement.setString(1, employee.getName());
+        statement.setString(2, employee.getJobName());
+        statement.setString(3, employee.getSalaryGrade());
+        statement.setString(4, employee.getDepartment());
+        try {
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new ResponseStatusException(HttpStatusCode.valueOf(400), "Could not create employee, please check all required fields are correct.");
+        }
+        int newId = 0;
+        try (ResultSet resultSet = statement.getGeneratedKeys()) {
+            if (resultSet.next()) {
+                newId = resultSet.getInt("id");
+            }
+        } catch (Exception e) {
+            System.out.println("Employee Creation: " + e);
+        }
+        employee.setId(newId);
+        return employee;
+    }
+
+    public Employee get(int id) throws SQLException {
+        PreparedStatement statement = this.connection.prepareStatement("SELECT * FROM employees WHERE id = ?");
+        statement.setInt(1, id);
+        ResultSet resultSet = statement.executeQuery();
+        Employee employee = null;
+        if (resultSet.next()) {
+            employee = new Employee(
+                    resultSet.getInt("id"),
+                    resultSet.getString("name"),
+                    resultSet.getString("jobName"),
+                    resultSet.getString("salaryGrade"),
+                    resultSet.getString("department")
+            );
+        }
+        return employee;
+    }
+
+    public Employee update(int id, Employee employee) throws SQLException {
+        String SQL = "UPDATE employees SET name = ?, jobName = ?, salaryGrade = ?, department = ? WHERE id = ?";
+        PreparedStatement statement = this.connection.prepareStatement(SQL);
+        statement.setString(1, employee.getName());
+        statement.setString(2, employee.getJobName());
+        statement.setString(3, employee.getSalaryGrade());
+        statement.setString(4, employee.getDepartment());
+        statement.setInt(5, id);
+        try {
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new ResponseStatusException(HttpStatusCode.valueOf(400), "Could not update the employee, please check all required fields are correct.");
+        }
+        return this.get(id);
+    }
+
+    public Employee delete(int id) throws SQLException {
+        PreparedStatement statement = this.connection.prepareStatement("DELETE FROM employees WHERE id = ?");
+        statement.setInt(1, id);
+        Employee theEmployee = this.get(id);
+        if (theEmployee == null) {
+            return null;
+        }
+        statement.executeUpdate();
+        return theEmployee;
     }
 }
