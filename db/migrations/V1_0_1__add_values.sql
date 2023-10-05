2cc80ff79ed56fc91df107b3f16d4caeeb27ba72
Kevin Huang <krythuang@gmail.com>
2023-10-05 21:23:57 +0200

    finished the core

---
+++ db/migrations/V1_0_1__add_values.sql
@@ -0,0 +1,8 @@
+INSERT INTO employees
+(name, jobName, salaryGrade, department)
+
+VALUES  ('Kevin Huang', 'Devops', 'Grade 2', 'IT'),
+        ('Ali Keskin', 'Security', 'Grade 2', 'IT'),
+        ('Veronika Miteva', 'Administrator', 'Grade 3', 'IT'),
+        ('Esther Buijnsters', 'Manager', 'Grade 1', 'IT'),
+        ('Ilias Kalamatas', 'Consultant', 'Manager2', 'Marketing');
