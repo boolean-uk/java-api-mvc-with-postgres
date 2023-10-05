6841d7d98385d6b678bf747069d79de6b12a9d17
Panagiotis Karapiperis <panagiotis_karapiperis@hotmail.com>
2023-10-05 10:26:40 +0300

    Panagiotis Karapiperis extension-departments get requests

--- src/main/java/com/booleanuk/api/department/DepartmentController.java
+++ src/main/java/com/booleanuk/api/department/DepartmentController.java
@@ -1,4 +1,36 @@
 package com.booleanuk.api.department;
 
+import com.booleanuk.api.employee.Employee;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
+@RequestMapping("departments")
 public class DepartmentController {
+    private DepartmentRepository departments;
+
+    public DepartmentController() throws SQLException {
+        departments = new DepartmentRepository();
+    }
+
+    @GetMapping
+    public List<Department> getAllDepartments() throws SQLException {
+        return this.departments.getAllDepartments();
+    }
+
+    @GetMapping("/{id}")
+    public Department getOneDepartment(@PathVariable(name = "id") int id) throws SQLException{
+        if (this.departments.getOneDepartment(id) == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } else {
+            return this.departments.getOneDepartment(id);
+        }
+    }
 }
--- src/main/java/com/booleanuk/api/department/DepartmentRepository.java
+++ src/main/java/com/booleanuk/api/department/DepartmentRepository.java
@@ -1,4 +1,80 @@
 package com.booleanuk.api.department;
 
+import com.booleanuk.api.employee.Employee;
+import org.postgresql.ds.PGSimpleDataSource;
+
+import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
 public class DepartmentRepository {
+    DataSource dataSource;
+    String dbUser;
+    String dbURL;
+    String dbPassword;
+    String dbDatabase;
+    Connection connection;
+
+    public DepartmentRepository() throws SQLException {
+        this.getDatabaseCredentials();
+        this.dataSource = this.createDataSource();
+        this.connection = this.dataSource.getConnection();
+    }
+
+    private void getDatabaseCredentials() {
+        try (InputStream input = new FileInputStream("src/main/resources/config.properties")) {
+            Properties prop = new Properties();
+            prop.load(input);
+            this.dbUser = prop.getProperty("db.user");
+            this.dbURL = prop.getProperty("db.url");
+            this.dbPassword = prop.getProperty("db.password");
+            this.dbDatabase = prop.getProperty("db.database");
+        } catch (Exception e) {
+            System.out.println("Database Credentials Failed To Load" + e);
+        }
+    }
+
+    private DataSource createDataSource() {
+        final String url = "jdbc:postgresql://" + this.dbURL + ":5432/" + this.dbDatabase + "?user=" + this.dbUser + "&password=" + this.dbPassword;
+        final PGSimpleDataSource dataSource1 = new PGSimpleDataSource();
+        dataSource1.setURL(url);
+        return dataSource1;
+    }
+
+    public List<Department> getAllDepartments() throws SQLException {
+        List<Department> departmentList = new ArrayList<>();
+        PreparedStatement statement = this.connection.prepareStatement("SELECT * FROM departments");
+        ResultSet results = statement.executeQuery();
+        while (results.next()) {
+            Department department = new Department(
+                    results.getInt("id"),
+                    results.getString("name"),
+                    results.getString("location")
+            );
+            departmentList.add(department);
+        }
+        return departmentList;
+    }
+
+    public Department getOneDepartment(int id) throws SQLException {
+        PreparedStatement statement = this.connection.prepareStatement("SELECT * FROM department WHERE id = ?");
+        statement.setInt(1,id);
+        ResultSet result = statement.executeQuery();
+        Department department = null;
+        if (result.next()) {
+            department = new Department(
+                    result.getInt("id"),
+                    result.getString("name"),
+                    result.getString("location")
+            );
+        }
+        return department;
+    }
 }
