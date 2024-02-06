c943540b5875df2b8af00486db3a673ba33704a4
Nicolai Klokmose <angryniczorn@gmail.com>
2024-02-06 19:57:32 +0100

    Controllers and Basic repositories for salaries and departments

--- src/main/java/com/booleanuk/api/department/DepartmentController.java
+++ src/main/java/com/booleanuk/api/department/DepartmentController.java
@@ -1,4 +1,26 @@
 package com.booleanuk.api.department;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
+@RequestMapping("departments")
 public class DepartmentController {
+    private DepartmentRepository departments;
+
+    public DepartmentController() throws SQLException {
+        this.departments = new DepartmentRepository();
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Department> getAll() throws SQLException {
+        return this.departments.getAll();
+    }
 }
