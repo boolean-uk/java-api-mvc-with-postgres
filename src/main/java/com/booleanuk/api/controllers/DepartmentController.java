4f327b45bad9b1f568be30f0fd909fe047d567fe
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-06 15:11:02 +0100

    Create getAll Departments

--- src/main/java/com/booleanuk/api/controllers/DepartmentController.java
+++ src/main/java/com/booleanuk/api/controllers/DepartmentController.java
@@ -1,10 +1,14 @@
 package com.booleanuk.api.controllers;
 
+import com.booleanuk.api.models.Department;
+import com.booleanuk.api.models.Employee;
 import com.booleanuk.api.repositories.DepartmentRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.SQLException;
+import java.util.List;
 
 @RestController
 @RequestMapping("departments")
@@ -13,5 +17,10 @@ public class DepartmentController {
 
     public DepartmentController() throws SQLException {
         this.departments = new DepartmentRepository();
+    }
+
+    @GetMapping
+    public List<Department> getAll() throws SQLException {
+        return this.departments.getAll();
     }
 }
--- src/main/java/com/booleanuk/api/repositories/DepartmentRepository.java
+++ src/main/java/com/booleanuk/api/repositories/DepartmentRepository.java
@@ -1,12 +1,18 @@
 package com.booleanuk.api.repositories;
 
+import com.booleanuk.api.models.Department;
+import com.booleanuk.api.models.Employee;
 import org.postgresql.ds.PGSimpleDataSource;
 
 import javax.sql.DataSource;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class DepartmentRepository {
@@ -41,5 +47,18 @@ public class DepartmentRepository {
         final PGSimpleDataSource dataSource = new PGSimpleDataSource();
         dataSource.setUrl(url);
         return dataSource;
+    }
+
+    public List<Department> getAll() throws SQLException  {
+        List<Department> everyone = new ArrayList<>();
+        PreparedStatement statement = this.connection.prepareStatement("SELECT * FROM Departments");
+
+        ResultSet results = statement.executeQuery();
+
+        while (results.next()) {
+            Department theDepartment = new Department(results.getInt("id"), results.getString("name"), results.getString("location"));
+            everyone.add(theDepartment);
+        }
+        return everyone;
     }
 }
