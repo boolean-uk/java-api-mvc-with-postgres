b5bbba5e5691cbfd5aa6b1247b5b78091844d1a5
Enock97 <enockladu@hotmail.com>
2024-02-06 14:00:40 +0100

    Completed core exercise.

---
+++ db/migrations/V2_0_0__populate_employee_table.sql
@@ -0,0 +1,8 @@
+INSERT INTO Employees
+(name, jobName, salaryGrade, department)
+VALUES
+('Ada Lovelace', 'Engineer','70', '2'),
+('Charles Cabbage', 'Engineer','70', '2'),
+('Grace Hopper', 'Secretary', '60', '3'),
+('Alan Turing', 'Software Developer', '80', '4'),
+('Katherine Johnson', 'CEO', '100', '1');
