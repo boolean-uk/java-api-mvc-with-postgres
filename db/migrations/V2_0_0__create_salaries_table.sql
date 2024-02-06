dbd94d4e716ee2e7953efcf26cdfcfd4a490add4
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-02-06 15:21:19 +0100

    Updated migrations set foreign keys in employees

--- db/migrations/V2_0_0__create_salaries_table.sql
+++ db/migrations/V2_0_0__create_salaries_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Salaries (
-id SERIAL PRIMARY KEY
+id SERIAL PRIMARY KEY,
 grade TEXT,
 minSalary INT,
 maxSalary INT
--- db/migrations/V2_4_0__update_foreign_key_for_salaries_in_employees.sql
+++ db/migrations/V2_4_0__update_foreign_key_for_salaries_in_employees.sql
@@ -1,11 +1,23 @@
 UPDATE Employees
 SET salary_id = 1
-WHERE id = 1 AND WHERE id = 3;
+WHERE id = 1;
+
+UPDATE Employees
+SET salary_id = 1
+WHERE id = 3;
 
 UPDATE Employees
 SET salary_id = 2
-WHERE id = 2 AND WHERE id = 6;
+WHERE id = 2;
+
+UPDATE Employees
+SET salary_id = 2
+WHERE id = 6;
 
 UPDATE Employees
 SET salary_id = 3
-WHERE id = 4 AND WHERE id = 5;
+WHERE id = 4;
+
+UPDATE Employees
+SET salary_id = 3
+WHERE id = 5;
