3bd6b9dcb402be0888cce1b89688c8733a679079
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-06 15:07:58 +0100

    Set up DepartmentController class

--- src/main/java/com/booleanuk/api/controllers/DepartmentController.java
+++ src/main/java/com/booleanuk/api/controllers/DepartmentController.java
@@ -1,4 +1,17 @@
 package com.booleanuk.api.controllers;
 
+import com.booleanuk.api.repositories.DepartmentRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+
+@RestController
+@RequestMapping("departments")
 public class DepartmentController {
+    private DepartmentRepository departments;
+
+    public DepartmentController() throws SQLException {
+        this.departments = new DepartmentRepository();
+    }
 }
