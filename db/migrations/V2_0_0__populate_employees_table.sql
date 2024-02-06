a665bc62b78d6a5ae97e219e7434f64a89e4b472
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-06 11:52:56 +0100

    Create db migrations for employee and sample data

---
+++ db/migrations/V2_0_0__populate_employees_table.sql
@@ -0,0 +1,6 @@
+INSERT INTO employees
+    (name, job_name, salary_grade, department)
+VALUES
+    ('Hans Hansen', 'Fishing', 'Highest', 'Boat'),
+    ('Smith Smitherson', 'Tech CEO', 'Lowest', 'Skyscraper'),
+    ('Paul Paulson', 'Janitor', 'Middle', 'School Basement');
