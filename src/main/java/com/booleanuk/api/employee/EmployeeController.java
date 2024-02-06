29a0ab737b57d4174739d0120596219a6fea6f16
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-06 12:45:29 +0100

    Set up EmployeeController

--- src/main/java/com/booleanuk/api/employee/EmployeeController.java
+++ src/main/java/com/booleanuk/api/employee/EmployeeController.java
@@ -1,4 +1,16 @@
 package com.booleanuk.api.employee;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+
+@RestController
+@RequestMapping
 public class EmployeeController {
+    private EmployeeRepository employees;
+
+    public EmployeeController() throws SQLException {
+        this.employees = new EmployeeRepository();
+    }
 }
