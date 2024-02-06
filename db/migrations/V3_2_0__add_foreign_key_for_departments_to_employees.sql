aca9caad7de21f0817165bc5c3bfe66a552c5e53
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-06 14:38:07 +0100

    Add NOT NULL to foreign keys in employees

--- db/migrations/V3_2_0__add_foreign_key_for_departments_to_employees.sql
+++ db/migrations/V3_2_0__add_foreign_key_for_departments_to_employees.sql
@@ -1,5 +1,5 @@
 ALTER TABLE employees
-ADD COLUMN department_id INT;
+ADD COLUMN department_id INT NOT NULL;
 
 ALTER TABLE employees
 ADD CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES departments(id);
