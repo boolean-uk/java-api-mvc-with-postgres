c37f99d67cd35f6c710c25faca4a42a2f6477a9c
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-02-06 14:10:46 +0100

    Error messages to EmployeeController

--- src/main/java/com/booleanuk/api/employees/EmployeeController.java
+++ src/main/java/com/booleanuk/api/employees/EmployeeController.java
@@ -27,11 +27,20 @@ public class EmployeeController {
         if (employee != null)
             return employee;
         else throw new ResponseStatusException(HttpStatus.NOT_FOUND,
-                "Computer says \"I can't find the employee :.( \"");
+                "No employees with that id were found");
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Employee add(@RequestBody Employee employee) throws SQLException {
+        if(employee.getName() == null
+        || employee.getJobName() == null
+        || employee.getSalaryGrade() == null
+        || employee.getDepartment() == null)
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "Could not create employee, please check all required fields are correct");
+
         Employee addedEmployee = employees.add(employee);
         if (addedEmployee != null)
             return addedEmployee;
@@ -41,11 +50,19 @@ public class EmployeeController {
 
     @PutMapping("{id}")
     public Employee update(@PathVariable int id, @RequestBody Employee employee) throws SQLException {
+        if(employee.getName() == null
+                || employee.getJobName() == null
+                || employee.getSalaryGrade() == null
+                || employee.getDepartment() == null)
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "Could not update employee, please check all required fields are correct");
+
         Employee updatedEmployee = employees.update(id, employee);
         if (updatedEmployee != null)
             return updatedEmployee;
         else throw new ResponseStatusException(HttpStatus.NOT_FOUND,
-                "Computer says \"I can't find the employee you tried to update :.( \"");
+                "No employee with that id was found");
     }
 
     @DeleteMapping("{id}")
@@ -54,6 +71,6 @@ public class EmployeeController {
         if (deletedEmployee != null)
             return deletedEmployee;
         else throw new ResponseStatusException(HttpStatus.NOT_FOUND,
-                "Computer says \"I can't find the employee you tried to delete :.( \" which sounds ironic but I wanted to see it one last time");
+                "No employee with that id was found");
     }
 }
