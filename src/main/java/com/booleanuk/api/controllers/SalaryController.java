3712576792280305f2f7d2edf203eea72422dbd1
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-06 15:32:07 +0100

    Set up SalaryController

--- src/main/java/com/booleanuk/api/controllers/SalaryController.java
+++ src/main/java/com/booleanuk/api/controllers/SalaryController.java
@@ -1,4 +1,17 @@
 package com.booleanuk.api.controllers;
 
+import com.booleanuk.api.repositories.SalaryRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+
+@RestController
+@RequestMapping("salaries")
 public class SalaryController {
+    private SalaryRepository salaries;
+
+    public SalaryController() throws SQLException {
+        this.salaries = new SalaryRepository();
+    }
 }
