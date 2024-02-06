9f77d998bcb8e77e13375401b7c19cde7ca2af3c
Petter Karstensen <petterkarstensen@gmail.com>
2024-02-06 19:00:26 +0100

    Get all employees

--- src/main/java/com/booleanuk/api/employee/EmployeeController.java
+++ src/main/java/com/booleanuk/api/employee/EmployeeController.java
@@ -1,4 +1,23 @@
 package com.booleanuk.api.employee;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
+@RequestMapping("employees")
 public class EmployeeController {
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeController() throws SQLException {
+        this.employeeRepository = new EmployeeRepository();
+    }
+
+    @GetMapping
+    public List<Employee> getAll() throws SQLException {
+        return this.employeeRepository.getAll();
+    }
 }
--- src/main/java/com/booleanuk/api/employee/EmployeeRepository.java
+++ src/main/java/com/booleanuk/api/employee/EmployeeRepository.java
@@ -1,8 +1,18 @@
 package com.booleanuk.api.employee;
 
+import org.postgresql.ds.PGSimpleDataSource;
+
 import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Properties;
 
 public class EmployeeRepository {
     DataSource dataSource;
@@ -12,11 +22,41 @@ public class EmployeeRepository {
     String dbDatabase;
     Connection connection;
 
-    public EmployeeRepository(){
+    public EmployeeRepository() throws SQLException {
+        this.getDatabaseCredentials();
+        this.dataSource = this.getDataSource();
+        this.connection = this.dataSource.getConnection();
 
     }
     private void getDatabaseCredentials(){
-
+        try (InputStream inputStream = new
+                FileInputStream("src/main/resources/config.properties")) {
+            Properties properties = new Properties();
+            properties.load(inputStream);
+            this.dbURL = properties.getProperty("db.url");
+            this.dbUser = properties.getProperty("db.user");
+            this.dbPassword = properties.getProperty("db.password");
+            this.dbDatabase = properties.getProperty("db.database");
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
     }
-
+    private DataSource getDataSource(){
+        String URL = this.dbURL + this.dbDatabase + "?user=" + this.dbUser + "&password=" +this.dbPassword;
+        final PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setUrl(URL);
+        return dataSource;
+    }
+    public List<Employee> getAll() throws SQLException{
+        List<Employee> allEmployees = new ArrayList<>();
+        PreparedStatement preparedStatement = this.connection.prepareStatement("SELECT * FROM employees");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            Employee employee = new Employee(resultSet.getInt("id"), resultSet.getString("name"),
+                    resultSet.getString("jobName"), resultSet.getString("salaryGrade"),
+                    resultSet.getString("department"));
+            allEmployees.add(employee);
+        }
+        return allEmployees;
+    }
 }
