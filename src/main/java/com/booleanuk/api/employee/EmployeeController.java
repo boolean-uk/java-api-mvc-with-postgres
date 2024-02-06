145d600eba0b6eae5d64cedc25f6b4af41afc26b
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-06 14:57:16 +0100

    Add exception response status to employee endpoints

--- src/main/java/com/booleanuk/api/employee/EmployeeController.java
+++ src/main/java/com/booleanuk/api/employee/EmployeeController.java
@@ -2,6 +2,7 @@ package com.booleanuk.api.employee;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -23,22 +24,44 @@ public class EmployeeController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Employee create(@RequestBody Employee employee) throws SQLException {
-        return this.employees.add(employee);
+        if (employee.getName() == null || employee.getJobName() == null || employee.getDepartment() == null || employee.getSalaryGrade() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad request");
+        }
+        Employee theEmployee =  this.employees.add(employee);
+        if (theEmployee == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad request");
+        }
+        return theEmployee;
     }
 
     @GetMapping("/{id}")
     public Employee get(@PathVariable(name = "id") int id) throws SQLException {
-        return this.employees.get(id);
+        Employee theEmployee =  this.employees.get(id);
+        if (theEmployee == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found");
+        }
+        return theEmployee;
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.CREATED)
     public Employee update(@PathVariable(name="id") int id, @RequestBody Employee employee) throws SQLException {
-        return this.employees.update(id, employee);
+        if (employee.getName() == null || employee.getJobName() == null || employee.getDepartment() == null || employee.getSalaryGrade() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad request");
+        }
+        Employee theEmployee = this.employees.update(id, employee);
+        if (theEmployee == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found");
+        }
+        return theEmployee;
     }
 
     @DeleteMapping("/{id}")
     public Employee delete(@PathVariable(name="id") int id) throws SQLException {
-        return this.employees.delete(id);
+        Employee theEmployee =  this.employees.delete(id);
+        if (theEmployee == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found");
+        }
+        return theEmployee;
     }
 }
