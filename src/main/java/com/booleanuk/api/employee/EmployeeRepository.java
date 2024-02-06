0bdf2cad9b66b3b53c2210213e9f70fe1fe91221
Alexander <alexander.ellnestam@gmail.com>
2024-02-06 21:02:34 +0100

    Done with exercise

--- src/main/java/com/booleanuk/api/employee/EmployeeRepository.java
+++ src/main/java/com/booleanuk/api/employee/EmployeeRepository.java
@@ -12,46 +12,47 @@ import java.util.Properties;
 
 public class EmployeeRepository {
 
-    private DataSource dataSource;
-    private String dbUser;
-    private String dbURL;
-    private String dbPassword;
-    private String dbDataBase;
-    private Connection connection;
+    DataSource dataSource;
+    String dbUser;
+    String dbURL;
+    String dbPassword;
+    String dbDataBase;
+    Connection connection;
 
-    public EmployeeRepository() throws SQLException{
+    public EmployeeRepository() throws SQLException {
+        this.getDatabaseCredentials();
         this.dataSource = this.createDataSource();
         this.connection = this.dataSource.getConnection();
-        this.getDatabaseCredentials();
+
 
     }
 
     private void getDatabaseCredentials() {
-        try(InputStream inputStream = new FileInputStream("src/main/resources/config.properties")){
+        try (InputStream inputStream = new FileInputStream("src/main/resources/config.properties")) {
             Properties properties = new Properties();
             properties.load(inputStream);
-            this.dbUser = properties.getProperty(dbUser);
-            this.dbURL = properties.getProperty(dbURL);
-            this.dbPassword = properties.getProperty(dbPassword);
-            this.dbDataBase = properties.getProperty(dbDataBase);
-        } catch (Exception e){
+            this.dbUser = properties.getProperty("db.user");
+            this.dbURL = properties.getProperty("db.url");
+            this.dbPassword = properties.getProperty("db.password");
+            this.dbDataBase = properties.getProperty("db.database");
+        } catch (Exception e) {
             System.out.println("Oh no: " + e);
         }
     }
 
     private DataSource createDataSource() {
-        final String url = "jdbc:postgressql://" + this.dbURL + ":5432/" + this.dbDataBase + "?user=" + this.dbUser + "&password=" + this.dbPassword;
+        final String url = "jdbc:postgresql://" + this.dbURL + ":5432/" + this.dbDataBase + "?user=" + this.dbUser + "&password=" + this.dbPassword;
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
-        dataSource.setURL(url);
+        dataSource.setUrl(url);
         return dataSource;
     }
 
-    public List<Employee> getAll() throws SQLException{
+    public List<Employee> getAll() throws SQLException {
         List<Employee> employeesList = new ArrayList<>();
         PreparedStatement preparedStatement = this.connection.prepareStatement("SELECT * FROM employee");
 
         ResultSet resultSet = preparedStatement.executeQuery();
-        while (resultSet.next()){
+        while (resultSet.next()) {
             Employee employee = new Employee(
                     resultSet.getInt("id"),
                     resultSet.getString("name"),
@@ -63,12 +64,12 @@ public class EmployeeRepository {
         return employeesList;
     }
 
-    public Employee getOne(int id) throws SQLException{
+    public Employee getOne(int id) throws SQLException {
         PreparedStatement preparedStatement = this.connection.prepareStatement("SELECT * FROM employee WHERE id=?");
-        preparedStatement.setInt(1,id);
+        preparedStatement.setInt(1, id);
         ResultSet resultSet = preparedStatement.executeQuery();
         Employee employee = null;
-        if(resultSet.next()){
+        if (resultSet.next()) {
             employee = new Employee(
                     resultSet.getInt("id"),
                     resultSet.getString("name"),
@@ -79,7 +80,7 @@ public class EmployeeRepository {
         return employee;
     }
 
-    public Employee add(Employee employee) throws SQLException{
+    public Employee add(Employee employee) throws SQLException {
         String SQL = "INSERT INTO employee(name, jobName, salaryGrade, department) VALUES(?, ?, ? , ?)";
         PreparedStatement preparedStatement = this.connection.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
         preparedStatement.setString(1, employee.getName());
@@ -88,22 +89,22 @@ public class EmployeeRepository {
         preparedStatement.setString(4, employee.getDepartment());
         int affectedRows = preparedStatement.executeUpdate();
         int id = 0;
-        if(affectedRows > 0){
-            try (ResultSet resultSet = preparedStatement.getGeneratedKeys()){
-                if(resultSet.next()){
+        if (affectedRows > 0) {
+            try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
+                if (resultSet.next()) {
                     id = resultSet.getInt(1);
                 }
-            } catch (Exception e){
+            } catch (Exception e) {
                 System.out.println("Oh no: " + e);
             }
             employee.setId(id);
-        }else {
+        } else {
             employee = null;
         }
         return employee;
     }
 
-    public Employee update(int id, Employee employee) throws SQLException{
+    public Employee update(int id, Employee employee) throws SQLException {
         String SQL = "UPDATE employee " + "SET name = ?, " + "jobName = ?, " + "salaryGrade = ?, " + "department = ?, " + "WHERE id = ?";
         PreparedStatement preparedStatement = this.connection.prepareStatement(SQL);
         preparedStatement.setString(1, employee.getName());
@@ -113,20 +114,20 @@ public class EmployeeRepository {
         preparedStatement.setInt(5, id);
         int affectedRows = preparedStatement.executeUpdate();
         Employee updateEmployee = null;
-        if (affectedRows > 0){
+        if (affectedRows > 0) {
             updateEmployee = this.getOne(id);
         }
         return updateEmployee;
     }
 
-    public Employee delete(int id) throws SQLException{
+    public Employee delete(int id) throws SQLException {
         String SQL = "DELETE FROM employee WHERE id=?";
         PreparedStatement preparedStatement = this.connection.prepareStatement(SQL);
         Employee deleteEmployee = null;
         deleteEmployee = this.getOne(id);
         preparedStatement.setInt(1, id);
         int affectedRows = preparedStatement.executeUpdate();
-        if (affectedRows == 0){
+        if (affectedRows == 0) {
             deleteEmployee = null;
         }
         return deleteEmployee;
