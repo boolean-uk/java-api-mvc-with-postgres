a654425e8bebbf5b9c269efea41ca13410263001
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-06 15:04:59 +0100

    Add department test

--- src/test/java/com/booleanuk/api/models/EmployeeTest.java
+++ src/test/java/com/booleanuk/api/models/EmployeeTest.java
@@ -8,6 +8,7 @@ class EmployeeTest {
     @Test
     public void testEmployeeConstructor() {
         Employee employee = new Employee(1, "Ex Ample", "Tester", "EXEMPLARY", "Testing");
+        Assertions.assertEquals(1, employee.getId());
         Assertions.assertEquals("Ex Ample", employee.getName());
         Assertions.assertEquals("Tester", employee.getJobName());
         Assertions.assertEquals("EXEMPLARY", employee.getSalaryGrade());
