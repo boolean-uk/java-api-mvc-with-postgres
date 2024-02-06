0fa9823ef25f3a389d2e910c8780fc0a8d5d55b2
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-02-06 13:49:07 +0100

    Created EmployeeRepository

--- src/main/java/com/booleanuk/api/requests/EmployeeController.java
+++ src/main/java/com/booleanuk/api/requests/EmployeeController.java
@@ -4,6 +4,7 @@ package com.booleanuk.api.requests;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @RestController
@@ -11,35 +12,42 @@ import java.util.List;
 public class EmployeeController {
     EmployeeRepository employeeRepository;
 
-    public EmployeeController() {
+    public EmployeeController() throws SQLException {
         employeeRepository = new EmployeeRepository();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Employee createEmployee(@RequestBody Employee employee) {
+    public Employee createEmployee(@RequestBody Employee employee) throws SQLException {
         Employee createdEmployee = this.employeeRepository.createEmployee(employee);
         return createdEmployee;    //Loopa igenom listan för att säkerställa att employee lagts till
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<Employee> getEmployees() {
+    public List<Employee> getEmployees() throws SQLException {
         List<Employee> allEmployees = this.employeeRepository.getEmployees();
         return allEmployees;
     }
 
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Employee getSpecificEmployee(@PathVariable long id) throws SQLException {
+        Employee specificEmployee = this.employeeRepository.getSpecificEmployee(id);
+        return specificEmployee;
+    }
+
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.CREATED)
-    public Employee updateEmployee(@PathVariable int id, @RequestBody Employee employee) {
+    public Employee updateEmployee(@PathVariable long id, @RequestBody Employee employee) throws SQLException {
         Employee updatedEmployee = this.employeeRepository.updateEmployee(id, employee);
         return updatedEmployee;
     }
 
-    @GetMapping("/{id}")
+    @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Employee getSpecificEmployee(@PathVariable int id) {
-        Employee deletedEmployee = this.employeeRepository.getSpecificEmployee(id);
+    public Employee deleteEmployee(@PathVariable long id) throws SQLException {
+        Employee deletedEmployee = this.employeeRepository.deleteEmployee(id);
         return deletedEmployee;
     }
 }
