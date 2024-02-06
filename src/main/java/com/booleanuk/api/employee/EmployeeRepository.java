7b34d7fe4bf31d67b44d92830ffe848dcaaa3b7a
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-06 12:05:02 +0100

    Set up db connection in EmployeeRepository

--- src/main/java/com/booleanuk/api/employee/EmployeeRepository.java
+++ src/main/java/com/booleanuk/api/employee/EmployeeRepository.java
@@ -1,4 +1,45 @@
 package com.booleanuk.api.employee;
 
+import org.postgresql.ds.PGSimpleDataSource;
+
+import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class EmployeeRepository {
+    DataSource datasource;
+    String dbUser;
+    String dbURL;
+    String dbPassword;
+    String dbDatabase;
+    Connection connection;
+
+    public EmployeeRepository() throws SQLException {
+        this.getDatabaseCredentials();
+        this.datasource = this.createDataSource();
+        this.connection = this.datasource.getConnection();
+    }
+
+    private void getDatabaseCredentials() {
+        try (InputStream input = new FileInputStream("src/main/resources/config.properties")) {
+            Properties prop = new Properties();
+            prop.load(input);
+            this.dbUser = prop.getProperty("db.user");
+            this.dbURL = prop.getProperty("db.url");
+            this.dbPassword = prop.getProperty("db.password");
+            this.dbDatabase = prop.getProperty("db.database");
+        } catch(Exception e) {
+            System.out.println("Oops: " + e);
+        }
+    }
+
+    private DataSource createDataSource() {
+        final String url = "jdbc:postgresql://" + this.dbURL + ":5432/" + this.dbDatabase + "?user=" + this.dbUser +"&password=" + this.dbPassword;
+        final PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setUrl(url);
+        return dataSource;
+    }
 }
