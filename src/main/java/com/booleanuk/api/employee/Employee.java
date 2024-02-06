eca9ffba61c7a3165c7a479d36d055add9ae1ec4
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-06 12:00:50 +0100

    Set up Employee class

--- src/main/java/com/booleanuk/api/employee/Employee.java
+++ src/main/java/com/booleanuk/api/employee/Employee.java
@@ -1,4 +1,37 @@
 package com.booleanuk.api.employee;
 
 public class Employee {
+    private int id;
+    private String name;
+    private String jobName;
+    private String salaryGrade;
+    private String department;
+
+    public Employee(int id, String name, String jobName, String salaryGrade, String department) {
+        this.id = id;
+        this.name = name;
+        this.jobName = jobName;
+        this.salaryGrade = salaryGrade;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getJobName() {
+        return jobName;
+    }
+
+    public String getSalaryGrade() {
+        return salaryGrade;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
