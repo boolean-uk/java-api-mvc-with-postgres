2db44294fde1401a41c3913a8b1fd47ffdcd6407
Alexandra Härnström <alex.hernstrom@gmail.com>
2024-02-06 14:45:47 +0100

    Added migrations for extension

--- db/migrations/V1_1_0__add_dummy_data_to_employees.sql
+++ db/migrations/V1_1_0__add_dummy_data_to_employees.sql
@@ -2,7 +2,7 @@ INSERT INTO Employees (name, jobName, salaryGrade, department)
 VALUES
 ('Elin Koto', 'Manager', 'ABC', 'HR'),
 ('Indiana Fromme', 'Developer', 'DEF', 'IT'),
-('Leo Romcom', 'Receptionist', 'ABC', 'Hires'),
+('Leo Romcom', 'Receptionist', 'ABC', 'HR'),
 ('Romulus Rex', 'CEO', 'AA', 'Management'),
 ('Kendra Jones', 'CTO', 'AA', 'Management'),
 ('Freya Tenn', 'Consultant', 'DEF', 'IT');
---
+++ db/migrations/V2_4_0__update_foreign_key_for_salaries_in_employees.sql
@@ -0,0 +1,11 @@
+UPDATE Employees
+SET salary_id = 1
+WHERE id = 1 AND WHERE id = 3;
+
+UPDATE Employees
+SET salary_id = 2
+WHERE id = 2 AND WHERE id = 6;
+
+UPDATE Employees
+SET salary_id = 3
+WHERE id = 4 AND WHERE id = 5;
