690936279fd3ebf1ec603865b9412c782332b210
Mads Gjengaar <mig.gjengaar@gmail.com>
2024-02-07 14:19:20 +0100

    Completed Core

---
+++ db/migrations/V1_1_0__insert_into_employee.sql
@@ -0,0 +1,5 @@
+INSERT INTO Employee(name, job_name, salary_grade, department)
+VALUES
+    ("Tim Apple", "CEO", "first", "IT"),
+    ("John Brown", "Programmer", "second", "IT"),
+    ("Mr.Bean", "Comedian", "third", "UNKNOWN")
